501fc5256ffee76d80a6f48a8474328302c50fab
Bautista Centorbi <bautii.centorbii@gmail.com>
2024-09-09 16:34:33 -0300

    .ENV URL

--- app/products/page.js
+++ app/products/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ItemList from '../components/ui/ItemList/ItemList'
 
 const getProducts = async() => {
-    const data = await fetch(`http://localhost:3000/api/productos`, {cache: 'no-cache'})
+    const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/productos`, {cache: 'no-cache'})
     const productos = await data.json()
     return productos
 }
