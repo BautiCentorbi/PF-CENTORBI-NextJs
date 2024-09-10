76e733b6dd066ebadc78b0ec41655ae6dbc5b44b
Bautista Centorbi <bautii.centorbii@gmail.com>
2024-09-10 18:56:26 -0300

    Test Fix 1.5.2

--- app/products/page.js
+++ app/products/page.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import ItemList from '@/app/components/ui/ItemList/ItemList'
+import { NextResponse } from 'next/server'
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api'
 
 const getProducts = async() => {
     const data = await fetch(`${apiUrl}/productos`, {cache: 'no-cache'})
     if (!data.ok) {
+        const errorText = await data.text()
+        console.log('Error response form api', errorText)
         throw new Error('Error fetching data')
     }
-    const productos = await data?.json()
-    return productos
+    const productos = await data.json()
+    return NextResponse.json(productos)
 }
 const Productos = async() => {
     const products = await getProducts()
